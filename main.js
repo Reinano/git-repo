0dc5fc572b792b267700032e906a20fd6b572b7a
Reinano <rtoktogonov@gmail.com>
2021-06-15 16:11:02 +0300

    another tree

---
+++ main.js
@@ -0,0 +1,25 @@
+//Первое задание
+let celcius = prompt('Введите градусы по Цельсию');
+let fahrenheit = (9 / 5) * celcius + 32;
+alert("Ваши " + celcius + " градусов по Цельсию равны " + fahrenheit + " градусов по фаренгейту");
+
+
+// Второе задание
+let name = prompt('Ваше имя');
+let age = prompt('Ваш возраст');
+let message = 'Доброго времени суток ';
+alert(message + "дорогой " + name);
+
+
+// Третье задание
+let userName = "Василий";
+let admin;
+admin = userName;
+alert(admin);
+
+//Четвертое задание
+let fourExs = 1000 + "108";
+alert(fourExs);
+
+let five = 5;
+let six = 5;
